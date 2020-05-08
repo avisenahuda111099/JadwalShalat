374d56091ecf21e1794358dd8bb5150fab7fd8f2
avisenahuda111099 <avisena.hudabf109@gmail.com>
2020-05-09 05:29:03 +0700

    m

---
+++ app/src/main/java/com/example/jadwalshalat/view/activity/DevsActivity.java
@@ -0,0 +1,19 @@
+package com.example.jadwalshalat.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.jadwalshalat.R;
+
+public class DevsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_devs);
+
+    }
+}
--- app/src/main/java/com/example/jadwalshalat/view/fragment/DBSholatFragment.java
+++ app/src/main/java/com/example/jadwalshalat/view/fragment/DBSholatFragment.java
@@ -1,5 +1,6 @@
 package com.example.jadwalshalat.view.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,14 +8,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.jadwalshalat.R;
+import com.example.jadwalshalat.view.activity.DevsActivity;
+import com.example.jadwalshalat.view.activity.ReadActivity;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DBSholatFragment extends Fragment {
+    private Button button_prof;
 
     public DBSholatFragment() {
         // Required empty public constructor
@@ -24,7 +29,15 @@ public class DBSholatFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_d_b_sholat, container, false);
+        View tampil = inflater.inflate(R.layout.fragment_d_b_sholat, container, false);
+        button_prof = tampil.findViewById(R.id.btn_prof);
+        button_prof.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), DevsActivity.class);
+                startActivity(intent);
+            }
+        });
+        return tampil;
     }
 }
